12d0a82ba9071643a5e05135033eb21c616375f1
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-07-16 12:23:35 +0530

    Create printing days.py

---
+++ printing days.py
@@ -0,0 +1,17 @@
+a=int(input('enter numbers from 1-7:'))
+if a==1:
+	print('monday')
+elif a==2:
+	print('tuesday')
+elif a==3:
+	print('wednesday')
+elif a==4:
+	print('thursday')
+elif a==5:
+	print('friday')
+elif a==6:
+	print('saturday')
+elif a==7:
+	print('sunday')
+else:
+	print('enter valid input')
