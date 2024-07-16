98c462d71ea97b4244fe6b589ea9e92799f45c80
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-07-16 12:26:54 +0530

    Create grades of students.py

---
+++ grades of students.py
@@ -0,0 +1,13 @@
+a=float(input('enter marks:'))
+if a>=90:
+	print('A grade')
+elif a>=80 and a<90:
+	print('B grade')
+elif a>=70 and a<80:
+	print('C grade')
+elif a>60 and a<70:
+	print('D grade')
+elif a>=50 and a<60:
+	print('E grade')
+else:
+	print('CANDIDATE IS FAILED')
