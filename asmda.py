d47be13bec1a9686714f04a6a5a528a29bc82c69
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-07-16 12:29:02 +0530

    Create asmda.py

---
+++ asmda.py
@@ -0,0 +1,7 @@
+a=int(input('enter a value:'))
+b=int(input('enter a value:'))
+print('addition',a+b)
+print('subtraction',a-b)
+print('multiplication',a*b)
+print('division',a/b)
+print('a to the power of b',a**b)
