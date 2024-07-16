88a07dfb1b2c84c9911c532727a6701f35f3d8c1
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-07-16 12:30:54 +0530

    Create largest of three.py

---
+++ largest of three.py
@@ -0,0 +1,9 @@
+a=int(input('enter a value:'))
+b=int(input('enter b value:'))
+c=int(input('enter c value:'))
+if a>b and a>c:
+ 	print('a is large number')
+elif b>a and b>c:
+	print('b is large number')
+else:
+	print('c is large number')
