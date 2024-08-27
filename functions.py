491f6a8e4395a1f24130db6186809afd288506c7
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-08-27 12:32:56 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+def Add(a,b):
+	print ('adition',a+b)
+def sub(a,b):
+	print ('subtraction',a-b)
+def mul(a,b):
+	print ('multiplication',a*b)
+def div(a,b):
+	print ('division',a/b)
+a=int (input("Enter  the a value: "))
+b=int(input("Enter the value of b: "))
+Add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
