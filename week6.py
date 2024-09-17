92d120ac5d9195494d3c31c711e5cac657474116
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-09-17 12:14:54 +0530

    Update week6.py

--- week6.py
+++ week6.py
@@ -52,3 +52,17 @@ a2='village'
 dict1[a2]=a1
 print(dict1)
 ###########################################################
+num = lambda x: x % 2
+a = int(input("Enter a value: "))
+if num(a) == 0:
+    print("X is even:", a)
+else:
+    print("X is odd:", a)
+##############################################################
+dict1 = {'name': 'koushik', 'roll': 23, 'age': 19}
+total_sum = 0
+for value in dict1.values():
+    if isinstance(value, (int, float)):
+        total_sum += value
+print("Sum of numeric values:", total_sum)
+############################################################
